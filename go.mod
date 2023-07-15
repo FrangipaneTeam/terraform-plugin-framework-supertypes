0d48fee761743cfac58f2ac61b6a2aba0912e5b4
Mickael Stanislas <contact@mickael-stanislas.com>
2023-07-15 13:16:38 +0200

    feat: add object and singleNested Types

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/FrangipaneTeam/terraform-plugin-framework-supertypes
 go 1.20
 
 require (
+	github.com/fatih/color v1.13.0
 	github.com/hashicorp/terraform-plugin-framework v1.3.2
 	github.com/hashicorp/terraform-plugin-go v0.18.0
 	github.com/iancoleman/strcase v0.3.0
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 )
--- template.go
+++ template.go
@@ -12,7 +12,16 @@ import (
 	"os"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/iancoleman/strcase"
+)
+
+var (
+	red    = color.New(color.FgRed)
+	green  = color.New(color.FgGreen)
+	blue   = color.New(color.FgBlue)
+	yellow = color.New(color.FgYellow)
+	grey   = color.New(color.FgHiBlack)
 )
 
 type templateInfos struct {
@@ -37,33 +46,63 @@ var templateNestedType string
 //go:embed templates/nested_value.go.tmpl
 var templateNestedValue string
 
+//go:embed templates/object_type.go.tmpl
+var templateObjectType string
+
+//go:embed templates/object_value.go.tmpl
+var templateObjectValue string
+
+//go:embed templates/single_nested_type.go.tmpl
+var templateSingleNestedType string
+
+//go:embed templates/single_nested_value.go.tmpl
+var templateSingleNestedValue string
+
 var (
-	// TODO "object", "single_nested"
-	typesSimple = []string{"string", "bool", "float64", "int64", "number"}
-	typesArray  = []string{"list", "set", "map"}
-	typesNested = []string{"list_nested", "set_nested", "map_nested"}
+	typesSimple      = []string{"string", "bool", "float64", "int64", "number", "object"}
+	typesArray       = []string{"list", "set", "map"}
+	typesNested      = []string{"list_nested", "set_nested", "map_nested"}
+	typeObject       = []string{"object"}
+	typeSingleNested = []string{"single_nested"}
 )
 
 func main() {
-	fmt.Println("generating types files...")
+	yellow.Println("Starting generation of supertypes")
+	yellow.Println("=================================")
 
+	blue.Println("-> Generating simple types")
 	// * Simple types
 	for _, t := range typesSimple {
 		generateTemplate(t, templateSimpleType, templateSimpleValue)
 	}
 
+	blue.Println("-> Generating array types")
 	// * Array types
 	for _, t := range typesArray {
 		generateTemplate(t, templateArrayType, templateArrayValue)
 	}
 
+	blue.Println("-> Generating nested types")
 	// * Nested types
 	for _, t := range typesNested {
 		generateTemplate(t, templateNestedType, templateNestedValue)
 	}
+
+	blue.Println("-> Generating object type")
+	// * Object types
+	for _, t := range typeObject {
+		generateTemplate(t, templateObjectType, templateObjectValue)
+	}
+
+	blue.Println("-> Generating single nested type")
+	// * Single nested types
+	for _, t := range typeSingleNested {
+		generateTemplate(t, templateSingleNestedType, templateSingleNestedValue)
+	}
 }
 
 func generateTemplate(typeName, templateType, templateValue string) {
+	grey.Printf("	* Generating %s ...", typeName)
 	infos := templateInfos{
 		TypeName: strcase.ToCamel(typeName),
 	}
@@ -114,9 +153,13 @@ func generateTemplate(typeName, templateType, templateValue string) {
 
 	if err := os.WriteFile(typeName+"_type.go", formattedTmplType, 0644); err != nil {
 		fmt.Printf("write to file error : %v\n", err)
+		red.Println("✘")
 	}
 
 	if err := os.WriteFile(typeName+"_value.go", formattedTmplValue, 0644); err != nil {
 		fmt.Printf("write to file error : %v\n", err)
+		red.Println("✘")
 	}
+
+	green.Println("✔")
 }
